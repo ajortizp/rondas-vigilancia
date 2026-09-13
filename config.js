974ff6b48b4b3e990657b4972fc9f8222b8861c6
ajortizp <alejandro.op9@gmail.com>
2026-09-13 08:15:41 -0300

    Conecta backend Google Sheets

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
-  DEMO_MODE: true,
-  API_URL: "",
+  DEMO_MODE: false,
+  API_URL: "https://script.google.com/macros/s/AKfycbyZs9lvDA7KLQ6vQl8OQhurAusAq1NEuqhpWP8JdVmLVjkZ9DfmDHXhWpFPFEymHaid/exec",
   DEMO_RUT: "11111111-1",
   DEMO_PIN: "1234",
   DEMO_NAME: "Usuario Piloto"
